0fa680ec006b997a39e7376127db2ee776812057
c_melchor <cah.melchor@gmail.com>
2020-11-16 10:14:02 -0800

    added Home component

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
+        <Route path="/about">{/* <About /> */}</Route>
         <Route exact path="/">
           <Home />
         </Route>
--- water-my-plants/src/index.js
+++ water-my-plants/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
