57589c9c350cb9571c7ec898e3a740cae5ec4a5f
c_melchor <cah.melchor@gmail.com>
2020-11-17 15:39:39 -0800

    dashboard styled

--- water-my-plants/src/components/Dashboard.js
+++ water-my-plants/src/components/Dashboard.js
@@ -1,52 +1,82 @@
-import React, { useEffect } from 'react'
-import { axiosWithAuth } from "../auth/axiosWithAuth"
+import React, { useEffect } from "react";
+import { axiosWithAuth } from "../auth/axiosWithAuth";
+import styled from "styled-components";
 
-function Dashboard({user, userPlants, setUser, setUserPlants, setPlants}) {
-  console.log(user)
+const StyledDiv = styled.div`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  background-color: #b8d5cd;
+`;
+
+const StyledPlantDiv = styled.div`
+  width: 30%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-radius: 5%;
+  text-align: center;
+  margin-top: 3%;
+`;
+
+function Dashboard({ user, userPlants, setUser, setUserPlants, setPlants }) {
+  console.log(user);
   useEffect(() => {
-    const id = localStorage.getItem("user_id")
+    const id = localStorage.getItem("user_id");
     axiosWithAuth()
       .get(`/api/account/${id}`)
       .then(res => {
-        setUser(res.data.user)
+        setUser(res.data.user);
       })
       .catch(err => {
-        console.log(err)
-      })
+        console.log(err);
+      });
 
     axiosWithAuth()
       .get(`/api/plants/users/${id}`)
       .then(res => {
-        setUserPlants(res.data)
-      })
+        setUserPlants(res.data);
+      });
     axiosWithAuth()
       .get(`/api/plants`)
       .then(res => {
-        setPlants(res.data)
-      })
-  }, [])
+        setPlants(res.data);
+      });
+  }, []);
 
   return (
-    <div>
+    <StyledDiv>
       {user !== null ? (
-        <div>
+        <StyledPlantDiv>
           <p>{user.username}</p>
+          <br />
           <p>{user.phone_number}</p>
+          <br />
           <p>{user.userId}</p>
-        </div>
-      ): <p>Loading ...</p>}
-      
-      <div className="plants">
+          <br />
+        </StyledPlantDiv>
+      ) : (
+        <p>Loading ...</p>
+      )}
+
+      <StyledDiv className="plants">
         {userPlants.map(item => {
           return (
-          <div key={item.nickname}>
-            <p>Nickname: {item.nickname} Species: {item.species} Frequency: {item.h2o_frequency}</p>
-          </div>
-          )
+            <StyledPlantDiv key={item.nickname}>
+              <p>
+                Nickname: {item.nickname} <br /> Species: {item.species} <br />
+                Frequency: {item.h2o_frequency}
+              </p>
+            </StyledPlantDiv>
+          );
         })}
-      </div>
-    </div>
-  )
+      </StyledDiv>
+    </StyledDiv>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
