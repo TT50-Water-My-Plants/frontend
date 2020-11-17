7f4144f01a32145f75f6ea9d9de417a52b95e40c
c_melchor <cah.melchor@gmail.com>
2020-11-16 16:47:53 -0800

    removed functions from app.js into register.js

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -9,66 +9,6 @@ import Header from "./js/components/Header";
 import Register from "./Register";
 
 function App() {
-  const intiailFormValues = {
-    username: "",
-    phone_number: "",
-    password: ""
-  };
-
-  const defaultErrors = {
-    username: "",
-    phone_number: "",
-    password: ""
-  };
-
-  const [formState, setFormState] = useState(intiailFormValues);
-  const [errorState, setErrorState] = useState(defaultErrors);
-  const [user, setUser] = useState([]);
-
-  const validate = event => {
-    yup
-      .reach(formSchema, event.target.name)
-      .validate(event.target.value)
-      .then(valid => {
-        console.log(event.target.value, "VALUE HERE");
-        setErrorState({ ...errorState, [event.target.name]: "" });
-      })
-      .catch(err => {
-        console.log(err);
-        setErrorState({ ...errorState, [event.target.name]: err.errors[0] });
-      });
-  };
-
-  const onChange = event => {
-    event.persist();
-    validate(event);
-
-    setFormState({
-      ...formState,
-      [event.target.name]: event.target.value
-    });
-  };
-
-  const onSubmit = event => {
-    event.preventDefault();
-    axios
-      .post(
-        "https://water-my-plants-tt50.herokuapp.com/api/auth/register",
-        formState
-      )
-      .then(res => {
-        console.log(res, "RES HERE!!!!");
-        setUser(arr => [...arr, res.data]);
-        setFormState({
-          username: "",
-          phone_number: "",
-          password: ""
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
   return (
     <div className="App">
       <Header />
