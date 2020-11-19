fdc620532c380aabd15bee6176e6b278e0aedd5d
c_melchor <cah.melchor@gmail.com>
2020-11-19 12:21:47 -0800

    yup edited

--- water-my-plants/src/formSchema.js
+++ water-my-plants/src/formSchema.js
@@ -5,8 +5,14 @@ const formSchema = yup.object().shape({
     .string()
     .min(2)
     .required("username is required"),
-  phone_number: yup.string().required("phone number is required"),
-  password: yup.string().required("a password is required")
+  phone_number: yup
+    .string()
+    .max(10)
+    .required("phone number is required"),
+  password: yup
+    .string()
+    .min(5)
+    .required("a password is required")
 });
 
 export default formSchema;
